f0d378cf883dd6c18f87553e504eb6a0d46c0911
Alexandre St-fort <solonoobhacker6767+@gmail.com>
2026-03-27 10:07:27 -0400

    Update argocd-installation.bash

--- argocd-installation.bash
+++ argocd-installation.bash
@@ -18,7 +18,7 @@ echo 'password': `kubectl -n argocd get secret argocd-initial-admin-secret -o js
 
 if [ $? -ne 0  ]; then
   sleep 10
-  kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+  echo 'password': `kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo`
 fi
 
 echo 'http port:' ` kubectl get svc -n argocd -o jsonpath="{.items[6].spec.ports[0].nodePort}"`
